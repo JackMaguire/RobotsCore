6fdf6bccf42141fa98e755c75d7263f64ef1d13b
Jack Maguire <jackmaguire1444@gmail.com>
2021-08-21 17:57:16 -0400

    more progress

--- include/robots_core/metaprotocols/smart_autopilot_spring2021.hh
+++ include/robots_core/metaprotocols/smart_autopilot_spring2021.hh
@@ -11,7 +11,7 @@ namespace robots_core{
 
 template< typename Renderer = NullRenderer >
 void
-loop_smart_autopilot(
+loop_smart_autopilot_spring2021(
   RobotsGame & game,
   Renderer & renderer
 ){
@@ -22,19 +22,32 @@ loop_smart_autopilot(
 
     ///////////////////
     // Deeper Recursion
-    bool const recursive_solution_found = run_recursive_search< 6 >( game, 7, renderer );
+    bool const recursive_solution_found =
+      run_recursive_search< 6 >( game, 7, renderer );
     if( recursive_solution_found ) continue;
 
     //////////////////
     // Stall For Time
-    if( stall_for_time( ms ) ) {
-      if( run_recursive_search< 6 >( ms, 7 ) ) continue;
+    bool const any_stall_steps_taken =
+      stall_for_time( game, renderer );
+    if( any_stall_steps_taken ) {
+      //run recursion again
+      bool const recursive_solution_found2 =
+	run_recursive_search< 6 >( game, 7, renderer );
+      if( recursive_solution_found2 ) continue;
     }
 
+    
+    /////////////////
+    // Maybe Teleport
     if( game_.n_safe_teleports_remaining() > 0 ) {
-      bool const game_over = handle_move( 0, 0, true );
-      if( game_over ) break;
+      MoveResult const tele_result = game.teleport();
+      if( tele_result == MoveResult::YOU_WIN_GAME or 
+	tele_result == MoveResult::YOU_LOSE ){
+	break;
+      }
     } else {
+      // Risky Moves?
       bool const game_over = loop_autopilot( -1, ms, 1 );
       if( game_over ) break;
     }
