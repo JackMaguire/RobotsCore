9b7a17e4a6c0f84e78f0d4ca9ba0d3c37fdc784b
Jack Maguire <jackmaguire1444@gmail.com>
2021-10-12 23:04:47 +0000

    silly |0

--- include/robots_core/pocket/pocket.hh
+++ include/robots_core/pocket/pocket.hh
@@ -2,7 +2,7 @@
 
 #include <robots_core/board.hh>
 #include <algorithm>
-
+#include <array>
 #include <iostream>
 
 namespace robots_core {
@@ -15,6 +15,15 @@ enum class CardinalPost : unsigned char
    LEFT,
    RIGHT
   };
+
+//for easy indexing
+// |0 gets optimized out
+unsigned char operator|(
+  CardinalPost const a,
+  unsigned char const b
+){
+  return (unsigned char)(a) | b;
+}
 
 enum class TerminalType : unsigned char
   {
@@ -30,6 +39,15 @@ enum class DiagonalQuadrant : unsigned char
    DOWN_RIGHT
   };
 
+//for easy indexing
+// |0 gets optimized out
+unsigned char operator|(
+  DiagonalQuadrant const a,
+  unsigned char const b
+){
+  return (unsigned char)(a) | b;
+}
+
 
 struct Post {
   Position pos;
@@ -41,7 +59,7 @@ struct Pocket {
 
   Position center;
 
-  std::array< Position, 4 > cardinal_posts;
+  std::array< Post, 4 > cardinal_posts;
 
   std::array< unsigned char, 4 > diagonal_offsets;
 
@@ -55,81 +73,89 @@ find_cardinal_posts( Board const & b ){
 
   //UP
   {
+    Post & post = posts[ (unsigned char) CardinalPost::UP ];
+
     Position p = h;
     while( true ){
       p.y += 1;
       if( not Board::position_is_in_bounds( p ) ){
 	p.y -= 1;
-	posts[ CardinalPost::UP ].type = TerminalType::OOB;
+	post.type = TerminalType::OOB;
 	break;
-      } else if( b.cell(p) == Occupant.ROBOT ){
+      } else if( b.cell(p) == Occupant::ROBOT ){
 	p.y -= 1;
-	posts[ CardinalPost::UP ].type = TerminalType::ROBOT;
+	post.type = TerminalType::ROBOT;
 	break;
       }
     }
-    posts[ CardinalPost::UP ].pos = p;
-    posts[ CardinalPost::UP ].distance = p.y - h.y;
+    post.pos = p;
+    post.distance = p.y - h.y;
   } //up
 
   //DOWN
   {
+    Post & post = posts[ (unsigned char) CardinalPost::DOWN ];
+
     Position p = h;
     while( true ){
       p.y -= 1;
       if( not Board::position_is_in_bounds( p ) ){
 	p.y += 1;
-	posts[ CardinalPost::DOWN ].type = TerminalType::OOB;
+	post.type = TerminalType::OOB;
 	break;
-      } else if( b.cell(p) == Occupant.ROBOT ){
+      } else if( b.cell(p) == Occupant::ROBOT ){
 	p.y += 1;
-	posts[ CardinalPost::DOWN ].type = TerminalType::ROBOT;
+	post.type = TerminalType::ROBOT;
 	break;
       }
     }
-    posts[ CardinalPost::DOWN ].pos = p;
-    posts[ CardinalPost::DOWN ].distance = h.y - p.y;
+    post.pos = p;
+    post.distance = h.y - p.y;
   } //down
 
   //LEFT
   {
+    Post & post = posts[ (unsigned char) CardinalPost::LEFT ];
+
     Position p = h;
     while( true ){
       p.x -= 1;
       if( not Board::position_is_in_bounds( p ) ){
 	p.x += 1;
-	posts[ CardinalPost::LEFT ].type = TerminalType::OOB;
+	post.type = TerminalType::OOB;
 	break;
-      } else if( b.cell(p) == Occupant.ROBOT ){
+      } else if( b.cell(p) == Occupant::ROBOT ){
 	p.x += 1;
-	posts[ CardinalPost::LEFT ].type = TerminalType::ROBOT;
+	post.type = TerminalType::ROBOT;
 	break;
       }
     }
-    posts[ CardinalPost::LEFT ].pos = p;
-    posts[ CardinalPost::LEFT ].distance = h.x - p.x;
+    post.pos = p;
+    post.distance = h.x - p.x;
   } //left
 
   //RIGHT
   {
+    Post & post = posts[ (unsigned char) CardinalPost::RIGHT ];
+
     Position p = h;
     while( true ){
       p.x += 1;
       if( not Board::position_is_in_bounds( p ) ){
 	p.x -= 1;
-	posts[ CardinalPost::RIGHT ].type = TerminalType::OOB;
+	post.type = TerminalType::OOB;
 	break;
-      } else if( b.cell(p) == Occupant.ROBOT ){
+      } else if( b.cell(p) == Occupant::ROBOT ){
 	p.x -= 1;
-	posts[ CardinalPost::RIGHT ].type = TerminalType::ROBOT;
+	post.type = TerminalType::ROBOT;
 	break;
       }
     }
-    posts[ CardinalPost::RIGHT ].pos = p;
-    posts[ CardinalPost::RIGHT ].distance = p.x - h.x;
+    post.pos = p;
+    post.distance = p.x - h.x;
   } //right
 
-  std::cout << "post distances are " << posts[ CardinalPost::UP ].distance << ", " << posts[ CardinalPost::RIGHT ].distance << ", " << posts[ CardinalPost::DOWN ].distance << ", and " << posts[ CardinalPost::LEFT ].distance << std::endl;
+  std::cout << "post distances are " << posts[ CardinalPost::UP|0 ].distance << ", " << posts[ CardinalPost::RIGHT|0 ].distance << ", " << posts[ CardinalPost::DOWN|0 ].distance << ", and " << posts[ CardinalPost::LEFT|0 ].distance << std::endl;
 
   return posts;
 }
@@ -162,7 +188,7 @@ calc_up_right_diagonal(
       positions.begin(), 
       positions.end(),
       [&pocket]( Position const & p ){
-	return p.y >= pocket.cardinal_posts[ CardinalPost::UP ].pos.y or p.x <= pocket.cardinal_posts[ CardinalPost::LEFT ].pos.x;
+	return p.y >= pocket.cardinal_posts[ CardinalPost::UP|0 ].pos.y or p.x <= pocket.cardinal_posts[ CardinalPost::LEFT|0 ].pos.x;
       }
     )
   );
@@ -170,7 +196,7 @@ calc_up_right_diagonal(
   
 
   if( positions.empty() ){
-    return pocket.cardinal_posts[ CardinalPost::UP ].distance + pocket.cardinal_posts[ CardinalPost::LEFT ].distance;
+    return pocket.cardinal_posts[ CardinalPost::UP|0 ].distance + pocket.cardinal_posts[ CardinalPost::LEFT|0 ].distance;
   }
   
   //get closest Robot
