0b00b3e138f3d37493ebd9e2186554f88256f9df
Jack Maguire <jack@med.unc.edu>
2021-12-21 11:29:49 -0500

    new unit test for visualizaiton compilation

--- tests/test_compile.hh
+++ tests/test_compile.hh
@@ -1,11 +1,12 @@
 // Include all of the headers that we want the compiler to test
 
-#include <robots_core/global.hh>
 #include <robots_core/asserts.hh>
 #include <robots_core/board.hh>
 #include <robots_core/forecasting.hh>
-#include <robots_core/null_renderer.hh>
 #include <robots_core/game.hh>
+#include <robots_core/global.hh>
+#include <robots_core/null_renderer.hh>
+#include <robots_core/visualization.hh.hh>
 
 #include <robots_core/strategy/recursion.hh>
 #include <robots_core/strategy/stall.hh>
