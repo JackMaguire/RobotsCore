30c0d9af710369c9f2c935a242808afa69b96c71
Jack Maguire <jack@med.unc.edu>
2021-12-22 10:39:14 -0500

    draw edges involving human

--- include/robots_core/visualization.hh
+++ include/robots_core/visualization.hh
@@ -3,22 +3,30 @@
 #include <string>
 #include <sstream>
 #include <vector>
-#include <unordered_map>
+//#include <unordered_map>
 
 #include <robots_core/board.hh>
 #include <robots_core/asserts.hh>
 
 namespace robots_core {
 
+struct VisNode {
+  Position pos;
+  char label = '?';
+  std::string rgb = "20,50,150";
+
+  std::string label_str() const {
+    return std::to_string( label );
+  }
+};
+
 struct VisSettings {
   // If true, draw single-character labels on each element/
   // 'H' for human, 'R' for robot, etc
   bool label_elements = true;
 
-  //                  | Position of node
-  //                  |         | Label for node
-  //                  |         |
-  //std::unordered_map< Position, char > extra_nodes;
+  // Set nodes and colors for extra nodes
+  std::vector< VisNode > extra_nodes;
 
   //                      | Position for node 1
   //                      |         | Position for node 2  
@@ -74,12 +82,12 @@ draw_elements(
 ) {
   for( sm_int i = 0; i < Board::WIDTH; ++i ){
     for( sm_int j = 0; j < Board::HEIGHT; ++j ){
-      auto const bx = i;
-      auto const by = Board::HEIGHT - j - 1;
-      auto const cx = i*PixPerCell + CircleOffset;
-      auto const cy = j*PixPerCell + CircleOffset;
-      auto const tx = i*PixPerCell + CircleOffset/2;
-      auto const ty = j*PixPerCell + (3*CircleOffset)/2;
+      int const bx = i;
+      int const by = Board::HEIGHT - j - 1;
+      int const cx = i*PixPerCell + CircleOffset;
+      int const cy = j*PixPerCell + CircleOffset;
+      int const tx = i*PixPerCell + CircleOffset/2;
+      int const ty = j*PixPerCell + (3*CircleOffset)/2;
       RC_ASSERT( Board::position_is_in_bounds( bx, by ) );
       switch( board.cell( bx, by ) ){
       case( Occupant::EMPTY ):
@@ -117,6 +125,63 @@ draw_elements(
   }
 }
 
+template< int PixPerCell, int CircleOffset, int CircleRadius, typename Ostream >
+void
+draw_extra_nodes( 
+  VisSettings const & settings,
+  Ostream & out
+) {
+  for( VisNode const & node : settings.extra_nodes ){
+    int const bx = node.pos.x;
+    int const by = node.pos.y;
+    int const i = bx;
+    int const j = Board::HEIGHT - by - 1;
+    int const cx = i*PixPerCell + CircleOffset;
+    int const cy = j*PixPerCell + CircleOffset;
+    int const tx = i*PixPerCell + CircleOffset/2;
+    int const ty = j*PixPerCell + (3*CircleOffset)/2;
+    RC_ASSERT( Board::position_is_in_bounds( bx, by ) );
+
+    out << "<circle stroke=\"black\" stroke-width=\"0\" fill=\"rgb(" + node.rgb + ")\" "
+      "r=\"" << CircleRadius << "\" "
+      "cx=\"" << cx << "\" "
+      "cy=\"" << cy << "\" />\n";
+
+    if( settings.label_elements ){
+      out << "<text x=\"" << tx << "\" y=\"" << ty << "\" class=\"small\">" + node.label_str() + "</text>\n";
+    }
+
+  }
+}
+
+template< int PixPerCell, int CircleOffset >
+int convert_for_line( int const v ){
+  return PixPerCell * v + CircleOffset;
+}
+
+template< int PixPerCell, int CircleOffset, typename Ostream >
+void
+draw_edges( 
+  VisSettings const & settings,
+  Ostream & out
+) {
+  for( auto const & pos_pair : settings.edges ){
+    Position const p1 = pos_pair.first;
+    Position const p2 = pos_pair.second;
+
+    int const p1_i = convert_for_line< PixPerCell, CircleOffset >( p1.x );
+
+    int const p1_j = convert_for_line< PixPerCell, CircleOffset >( Board::HEIGHT-p1.y-1 );
+
+    int const p2_i = convert_for_line< PixPerCell, CircleOffset >( p2.x );
+
+    int const p2_j = convert_for_line< PixPerCell, CircleOffset >( Board::HEIGHT-p2.y-1 );
+
+    out << "<line x1=\"" << p1_i << "\" y1=\"" << p1_j << "\" x2=\"" << p2_i << "\" y2=\"" << p2_j << "\" style=\"stroke:rgb(0,0,0);stroke-width:1\" />\n";
+  }
+}
+
+
 template< typename Ostream >
 void
 to_svg( 
@@ -150,65 +215,10 @@ to_svg(
   // Header
   out << header << '\n';
 
-/*
-  // Background
-  //std::string const c1 = "dcdcdc";
-  //std::string const c2 = "c8c8c8";
-  bool use_c1 = false;
-  for( sm_int i = 0; i < Board::WIDTH; ++i ){
-    for( sm_int j = 0; j < Board::HEIGHT; ++j ){
-      use_c1 = !use_c1;
-      if( not use_c1 ){
-	out << "<rect fill=\"rgb(200,200,200)\" width=\"" << PixPerCell << "\" height=\"" << PixPerCell << "\" "
-	  "x=\"" << i*PixPerCell << "\" y=\"" << j*PixPerCell << "\" />\n";
-      }
-
-      // Elements
-      auto const bx = i;
-      auto const by = Board::HEIGHT - j - 1;
-      auto const cx = i*PixPerCell + CircleOffset;
-      auto const cy = j*PixPerCell + CircleOffset;
-      auto const tx = i*PixPerCell + CircleOffset/2;
-      auto const ty = j*PixPerCell + (3*CircleOffset)/2;
-      RC_ASSERT( Board::position_is_in_bounds( bx, by ) );
-      switch( board.cell( bx, by ) ){
-      case( Occupant::EMPTY ):
-      case( Occupant::OOB ):
-	break;
-      case( Occupant::ROBOT ):
-	out << "<circle stroke=\"black\" stroke-width=\"0\" fill=\"black\" "
-	  "r=\"" << CircleRadius << "\" "
-	  "cx=\"" << cx << "\" "
-	  "cy=\"" << cy << "\" />\n";
-	if( settings.label_elements ){
-	  out << "<text x=\"" << tx << "\" y=\"" << ty << "\" class=\"small\">R</text>\n";
-	}
-	break;
-      case( Occupant::FIRE ):
-	out << "<circle stroke=\"black\" stroke-width=\"0\" fill=\"red\" "
-	  "r=\"" << CircleRadius << "\" "
-	  "cx=\"" << cx << "\" "
-	  "cy=\"" << cy << "\" />\n";
-	if( settings.label_elements ){
-	  out << "<text x=\"" << tx << "\" y=\"" << ty << "\" class=\"small\">X</text>\n";
-	}
-	break;
-      case( Occupant::HUMAN ):
-	out << "<circle stroke=\"black\" stroke-width=\"0\" fill=\"green\" "
-	  "r=\"" << CircleRadius << "\" "
-	  "cx=\"" << cx << "\" "
-	  "cy=\"" << cy << "\" />\n";
-	if( settings.label_elements ){
-	  out << "<text x=\"" << tx << "\" y=\"" << ty << "\" class=\"small\">H</text>\n";
-	}
-	break;
-      }
-    }
-    use_c1 = !use_c1;
-  }
-  */
   draw_background< PixPerCell, CircleOffset, CircleRadius >(out);
+  draw_edges< PixPerCell, CircleOffset >( settings, out );
   draw_elements< PixPerCell, CircleOffset, CircleRadius >( board, settings, out );
+  draw_extra_nodes< PixPerCell, CircleOffset, CircleRadius >( settings, out );
 
   // Footer
   out << "</svg>\n";
--- test_vis.cc
+++ test_vis.cc
@@ -2,10 +2,55 @@
 
 #include <robots_core/game.hh>
 #include <robots_core/visualization.hh>
+#include <robots_core/graph/dense.hh>
 
 //Example: g++ test_vis.cc -o test_vis -O3 -Wall -Wextra -Iinclude -std=c++17
 
+using namespace robots_core;
+
+int
+get_human_node_ind(
+  graph::DenseGraph const & dg
+){
+  for( unsigned int i = 0; i < dg.cached_nodes.size(); ++i ){
+    //if( dg.cached_nodes[i].occupant == Occupant::HUMAN ){
+    if( dg.cached_nodes[i].special_case == graph::SpecialCaseNode::S ){
+      return i;
+    }
+  }
+  RC_ASSERT( false );
+  return 0;
+}
+
+void
+add_all_edges_to_human(
+  RobotsGame const & game,
+  VisSettings & vs
+){
+  graph::DenseGraph dg( game );
+  int const human_i = get_human_node_ind( dg );
+  Position const human_pos = dg.cached_nodes[ human_i ].position;
+
+  auto const & human_A_slice = dg.a[ human_i ];
+  for( unsigned int i = 0; i < human_A_slice.size(); ++i ){
+    if( human_A_slice[i] > 0 ){
+      //std::cout << "<!-- Edge with: " << i << " -->\n";
+      vs.edges.push_back({ human_pos, dg.cached_nodes[ i ].position });
+    }
+  }
+}
+
 int main(){
-  robots_core::Board b;
-  std::cout << robots_core::to_svg_string( b ) << std::endl;
+  RobotsGame const game;
+  Board const & b = game.board();
+
+  VisSettings vs;
+  vs.label_elements = false;
+  //vs.extra_nodes.push_back({ Position{ 1, 1 }, '7', "20,50,150" });
+  //vs.edges.push_back({ Position{ 1, 1 }, Position{ 2, 2 } });
+  //vs.edges.push_back({ Position{ 1, 1 }, Position{ 2, 1 } });
+
+  add_all_edges_to_human( game, vs );
+
+  std::cout << to_svg_string( b, vs ) << std::endl;
 }
